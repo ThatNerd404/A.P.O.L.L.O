11031fb0077e28f2fcdd5d2be95d54aa100a9e81
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2024-12-22 14:22:55 -0600

    changing voice values and adding stop to speak function

--- APOLLO.py
+++ APOLLO.py
@@ -4,6 +4,8 @@ import pyttsx3
 class Apollo():
     def __init__(self):
         self.engine = pyttsx3.init()
+        voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice',voices[0].id) #? sets voice to gender 0 for male 1 for female
         
     def math(self):
         pass
@@ -11,3 +13,4 @@ class Apollo():
     def speak(self, speech):
         self.engine.say(speech)
         self.engine.runAndWait()
+        self.engine.stop()
