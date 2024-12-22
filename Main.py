73fe7a97534df4dc3c1e49776d15076634fde7be
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2024-12-22 14:02:23 -0600

    getting rid of unnecessary imports

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
-import speech_recognition as sr
-import pyttsx3
+#import speech_recognition as sr
+
 from APOLLO import Apollo
 
 def main():
     Ap = Apollo()
     Ap.speak("Penis fart")
+    
 if __name__ == "__main__":
     main()
