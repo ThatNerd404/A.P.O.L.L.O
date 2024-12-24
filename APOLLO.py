a1f17d46de55109bd182c19982e835724fe21b35
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2024-12-24 14:53:06 -0600

    adding math function and main loop beginings

--- APOLLO.py
+++ APOLLO.py
@@ -4,12 +4,18 @@ import pyttsx3
 class Apollo():
     def __init__(self):
         self.engine = pyttsx3.init()
-        voices = self.engine.getProperty('voices')
-        self.engine.setProperty('voice',voices[0].id) #? sets voice to gender 0 for male 1 for female
-        
-    def math(self):
+        self.voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice',self.voices[0].id) #? sets voice to gender 0 for male 1 for female
+        self.engine.setProperty('volume',1.0) #? sets voice from 100 (1.0) to 1 (0.01)
+        self.math()
+    
+    def main_loop(self):
         pass
     
+    def math(self):
+        answer = str(5 + 5)
+        self.speak(f"5 + 5 equals {answer}")
+        
     def speak(self, speech):
         self.engine.say(speech)
         self.engine.runAndWait()
--- Main.py
+++ Main.py
@@ -4,7 +4,6 @@ from APOLLO import Apollo
 
 def main():
     Ap = Apollo()
-    Ap.speak("Penis fart")
     
 if __name__ == "__main__":
     main()
