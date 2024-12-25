7a90002ad4d30442ac04b5344aee48e680b8e946
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2024-12-25 15:44:22 -0600

    adding main loop function and figuring out how it will work

--- APOLLO.py
+++ APOLLO.py
@@ -3,18 +3,32 @@ import pyttsx3
 
 class Apollo():
     def __init__(self):
+        self.app_is_running = True
         self.engine = pyttsx3.init()
         self.voices = self.engine.getProperty('voices')
         self.engine.setProperty('voice',self.voices[0].id) #? sets voice to gender 0 for male 1 for female
         self.engine.setProperty('volume',1.0) #? sets voice from 100 (1.0) to 1 (0.01)
-        self.math()
-    
+        self.speak(f"Hello, I am APOLLO! I am your voice assistant! Ask away!")
+        self.main_loop()
+        
     def main_loop(self):
-        pass
-    
+        while self.app_is_running:
+            user_input = input("")
+            
+            if user_input.lower() == "quit" or user_input.lower() == "q":
+                self.speak(f"Bye Asshole")
+                self.app_is_running = False
+            
+            elif user_input.lower() == "math":
+                self.math()
+           
+            else:
+                self.speak(f"You haven't programmed me to do anything yet fuck wad.")
+                
     def math(self):
-        answer = str(5 + 5)
-        self.speak(f"5 + 5 equals {answer}")
+        self.speak(f"Type Your question?")
+        question = input()
+        self.speak(f"I have no clue how to answer that jackass! how about you program me to do that!")
         
     def speak(self, speech):
         self.engine.say(speech)
