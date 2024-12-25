0337455612d1743b5b8ee19660bb3995a7d7984f
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2024-12-25 17:37:42 -0600

    adding the hearing section

--- APOLLO.py
+++ APOLLO.py
@@ -1,36 +1,47 @@
 import numpy
 import pyttsx3
+import speech_recognition as sr 
+import openai
 
 class Apollo():
     def __init__(self):
         self.app_is_running = True
+        self.audio_recognizer = sr.Recognizer()
         self.engine = pyttsx3.init()
-        self.voices = self.engine.getProperty('voices')
-        self.engine.setProperty('voice',self.voices[0].id) #? sets voice to gender 0 for male 1 for female
-        self.engine.setProperty('volume',1.0) #? sets voice from 100 (1.0) to 1 (0.01)
+        
         self.speak(f"Hello, I am APOLLO! I am your voice assistant! Ask away!")
         self.main_loop()
         
     def main_loop(self):
         while self.app_is_running:
-            user_input = input("")
             
-            if user_input.lower() == "quit" or user_input.lower() == "q":
-                self.speak(f"Bye Asshole")
-                self.app_is_running = False
-            
-            elif user_input.lower() == "math":
-                self.math()
-           
-            else:
-                self.speak(f"You haven't programmed me to do anything yet fuck wad.")
+                self.listen()
+                if self.user_input == "quit" or self.user_input == "cancel" or self.user_input == "deactivate":
+                    self.speak("Powering ")
+                    break
+                
+                else:
+                    self.speak(f"User said: {self.user_input}")
                 
     def math(self):
-        self.speak(f"Type Your question?")
-        question = input()
-        self.speak(f"I have no clue how to answer that jackass! how about you program me to do that!")
+        pass
+    
+    def listen(self):
+        with sr.Microphone() as source:
+            self.speak("Listening. Hurry up!")
+            self.audio = self.audio_recognizer.listen(source)
         
+        try:
+            self.user_input = self.audio_recognizer.recognize_google(self.audio)
+            
+        except sr.UnknownValueError:
+            self.speak("Sorry, I didn't understand that.")
+            
     def speak(self, speech):
+        self.voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice',self.voices[0].id) #? sets voice to gender 0 for male 1 for female
+        self.engine.setProperty('volume',1.0) #? sets voice from 100 (1.0) to 1 (0.01)
+        
         self.engine.say(speech)
         self.engine.runAndWait()
         self.engine.stop()
