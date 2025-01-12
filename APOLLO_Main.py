32fa31646e077bc8bc560fa88dc9a82019a4755e
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2025-01-12 15:16:36 -0600

    Revert "test RAG working"
    This reverts commit 5a79d81efdad36692d069489f079972727632645.

--- APOLLO_Main.py
+++ APOLLO_Main.py
@@ -8,23 +8,8 @@ warnings.filterwarnings(
 class Apollo():
     def __init__(self):
         self.convo_history = ""
-        directory = "Documents"
-        documents = load_and_split_documents(directory)
-        #print(documents[0])
-        embeddings = generate_embeddings(documents)
-        text_list = [doc.page_content for doc in documents]  # Create a list of (text, embedding) tuples
-        text_embedding_pairs = list(zip(text_list, embeddings))
-        self.embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
-        self.retriever = FAISS.from_embeddings(text_embeddings= text_embedding_pairs, embedding=self.embedding_model)
-        prompt = PromptTemplate(input_variables=["context","documents", "personality", "convo_history", "query"], template=template)
-        self.qa_chain = RetrievalQA.from_chain_type(
-        llm=model, 
-        chain_type="stuff",  # Use "stuff" to stuff the documents directly into the prompt
-        retriever= self.retriever.as_retriever(),
-        return_source_documents=False,  # Optional: retrieve source documents as well
-        chain_type_kwargs={"prompt": prompt},
-)
-        #self.qa_chain.prompts["question"] = prompt
+        self.prompt = ChatPromptTemplate.from_template(template)
+        self.chain = self.prompt | model
         
     def run(self):
         print("Hello! I am A.P.O.L.L.O which stands for Automated Personalized Operations for Learning and Life Organization. Ask me anything and I will answer to the best of my ability.")
@@ -36,9 +21,9 @@ class Apollo():
     def call_ai(self,user_prompt):
         if user_prompt == "quit":
             sys.exit()
-        
+            
         self.start_time = time.time()
-        self.result = self.qa_chain.invoke({"context":context, "documents": self.retriever, "personality": personality, "convo_history": self.convo_history,"query":user_prompt})
+        self.result = self.chain.invoke({"context":context,"personality": personality, "convo_history": self.convo_history, "question" : user_prompt})
         self.end_time = time.time()
         self.total_time = self.end_time - self.start_time
         
