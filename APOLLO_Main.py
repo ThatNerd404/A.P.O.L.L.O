c8d50e7fbd270eed09b11f0dc6e74b254a13bffa
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2025-01-19 13:13:14 -0600

    adding comments and fixing spaces and junk

--- APOLLO_Main.py
+++ APOLLO_Main.py
@@ -9,7 +9,7 @@ warnings.filterwarnings(
 class Apollo:
     def __init__(self):
         self.speak("Initializing Systems.")
-        
+
         self.convo_history = ""
         self.prompt = ChatPromptTemplate.from_template(template)
         self.model = OllamaLLM(model="phi3:3.8b")
@@ -17,12 +17,12 @@ class Apollo:
         self.v_store = create_faiss_store(self.documents)
         self.vector_store = self.v_store
         self.chain = self.prompt | self.model
-        
+
         self.speak("Initializing Complete.")
-        
+
     def listen_for_wakeword(self, source):
         while True:
-            audio =  audio_recognizer.listen(source)
+            audio = audio_recognizer.listen(source)
             try:
                 user_audio = audio_recognizer.recognize_whisper(audio)
                 if "apollo" in user_audio.lower():
@@ -31,44 +31,44 @@ class Apollo:
                     self.speak(random_greeting)
                     self.listen_and_respond(source)
                     break
-                
+
             except sr.UnknownValueError:
                 pass
-            
-    def listen_and_respond(self,source):
+
+    def listen_and_respond(self, source):
         while True:
-            
             try:
                 audio = audio_recognizer.listen(source, timeout=5)
                 print("Listening...")
                 user_audio = audio_recognizer.recognize_whisper(audio)
-                print(f"Apollo heard {user_audio}")
-                
+                print(f"Apollo heard: {user_audio}")
+
                 if "quit" in user_audio:
                     sys.exit()
-                
                 elif not user_audio:
                     continue
-                
-                if user_audio == "" or user_audio == None:
+                elif not audio:
                     print("Silence found, shutting up, listening for wake word ...")
                     self.listen_for_wakeword(source)
-                
+
                 Ai_response = self.call_ai(user_audio)
                 print(Ai_response)
                 self.speak(Ai_response)
-                
+                self.convo_history += f"User: {user_audio} Apollo: {Ai_response}"
+
             except sr.UnknownValueError:
                 print("Silence found, shutting up, listening for wake word ...")
                 self.listen_for_wakeword(source)
-                
+                break
+
             except sr.WaitTimeoutError:
                 print("Silence found, shutting up, listening for wake word ...")
                 self.listen_for_wakeword(source)
                 break
-        
+
     def get_relevant_context(self, query):
 
+        # ? speed goes down the more k (the amount of chunks grabbed) goes up
         results = self.vector_store.similarity_search_with_score(query, k=1)
         relevant_docs = []
         for result, score in results:
@@ -90,7 +90,7 @@ class Apollo:
 
         self.end_time = time.time()
         self.total_time = self.end_time - self.start_time
-        
+
         print(f"Answer took about: {self.total_time} seconds!")
         return result
 
@@ -102,12 +102,11 @@ class Apollo:
     def run(self):
         print("Hello! I am A.P.O.L.L.O (Automated Personalized Operations for Learning and Life Organization).")
         print("Ask me anything, and I'll answer to the best of my ability. Type 'quit' to exit.")
-        
+
         while True:
             with sr.Microphone() as source:
                 self.listen_for_wakeword(source)
-            
-            
+
 
 if __name__ == "__main__":
     ap = Apollo()
