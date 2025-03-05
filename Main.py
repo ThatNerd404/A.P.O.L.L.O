90810fcd91e6d85b6895137ff08e47d1753cd49c
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2025-03-05 06:29:20 -0600

    changing main to also work if it is a exe file

--- Main.py
+++ Main.py
@@ -1,9 +1,17 @@
 from PySide6 import QtWidgets
 from User_Interface import UserInterface
 import sys
-import subprocess
+import multiprocessing
 import os 
+import subprocess
+import importlib
 
+def start_ollama():
+    subprocess.Popen(
+        ["ollama", "serve"],
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, creationflags=subprocess.CREATE_NO_WINDOW
+    )
+    
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = UserInterface()
@@ -13,14 +21,20 @@ def main():
 
 if __name__ == "__main__":
     # ? makes it not print to screen when starting process
-    ollama_server = subprocess.Popen(    ["ollama", "serve"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    multiprocessing.freeze_support()
+    multiprocessing.set_start_method("spawn", force=True)  # Prevent infinite subprocess spawns
+
+    ollama_process = multiprocessing.Process(target=start_ollama)
+    ollama_process.start()
     main()
+    ollama_process.terminate()
     #! work around because ollama_server.kill fails to cancel the server
     os.system("taskkill /F /IM ollama.exe > nul 2>&1")
     
 #TODO Make model to use for my job in lead generation
 # * command to turn ui files into py files: pyside6-uic APOLLO_Mainwindow.ui > APOLLO_MainWindow.py make sure to be in powershell
-# * command to turn py files into exe files: pyinstaller --onefile -w pytoexe.py
+# * command to turn py files into exe files: pyinstaller --onefile --noconsole pytoexe.py
 #! remember to change encoding of the  ui-to-py files to utf-8 EVERY SINGLE TIME
 #! REMEMBER YOU HAVE TO DELETE THE OLD PY FILE TO MAKE THE NEW ONE
 #! Remember to make executable for yourself make sure to have absolute path or just keep it where it is and add idk a shortcut
